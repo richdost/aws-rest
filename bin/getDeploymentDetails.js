487d0a0036a1c47b72ccc8a3302fb56db375006b
Richard Dost <Richard.Dost@gmail.com>
2018-05-07 12:43:28 -0700

    bin for getting REST endpoint

---
+++ bin/getDeploymentDetails.js
@@ -0,0 +1,8 @@
+#! /usr/bin/env node
+
+// todo make work with file argument
+// todo maybe combine with destroy for one? e.g. rest create config.js
+
+let awsRest = require('../index');
+//let config = require('../test/config.js'); // temp because TODO as file parameter
+return awsRest.getDeploymentDetails();
