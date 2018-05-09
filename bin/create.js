b38b85d19a520ef75af25ca33450fed65319dcc3
Richard Dost <Richard.Dost@gmail.com>
2018-05-08 21:21:21 -0700

    __dirname based paths for use as dependency

--- bin/create.js
+++ bin/create.js
@@ -1,18 +1,21 @@
 #! /usr/bin/env node
 
-// todo make work with file argument
 // todo maybe combine with destroy for one? e.g. rest create config.js
-
 let awsRest = require('../index');
 let fs = require('fs-extra');
-let configFile = process.argv[2];
+let path = require('path');
+let configFilePath = process.argv[2];
 
 let config;
 try {
-  config = fs.readJsonSync(configFile);
+  configFilePath = path.isAbsolute(configFilePath)
+    ? configFilePath
+    : process.cwd() + '/' + configFilePath;
+  console.log('path:'+configFilePath);
+  config = fs.readJsonSync(configFilePath);
 }
 catch (e) {
-  console.error('Error reading JSON config file: ' + configFile);
+  console.error('Error reading JSON config file: ' + configFilePath);
   console.log('Usage: create-rest <configFile>');
   process.exit(1);
 }
--- src/main.js
+++ src/main.js
@@ -5,22 +5,28 @@ let fs = require('fs-extra');
 
 // TODO tests - will need to use Claudia parameters for where to put/get Claudia.json
 
-const CONFIG_FILE = './rest/config.json';
-const CLAUDIA_FILE = './build/claudia.json';
+console.log('__dirname:' + __dirname);
+
+const BASE_PATH = __dirname + '/../';   // __dirname points to aws/bin
+const CONFIG_FILE = BASE_PATH + 'rest/config.json';
+const BUILD_DIR = BASE_PATH + 'build';
+const CLAUDIA_FILE = BASE_PATH + 'build/claudia.json';
+const CLAUDIA_LAMBDA_PROJECT = BASE_PATH + 'rest';
+const CLAUDIA_LAMBDA_POLICIES = BASE_PATH + 'policies';
 
 async function create(config) {
   if (!util.isValidConfig(config)) return Promise.reject('create received invalid config parameter');
-  await fs.ensureDir('./build');
+  await fs.ensureDir(BUILD_DIR);
   await fs.writeJson(CONFIG_FILE, config);
   util.initAWS(config);
   await util.createTables(config);
   const result = await claudia.create({  // https://github.com/claudiajs/claudia/blob/master/docs/create.md
-    source: './rest',
+    source: CLAUDIA_LAMBDA_PROJECT,
     config: CLAUDIA_FILE,
     region: config.aws.region || 'us-east-1',
     version: 'development',
     'api-module': 'rest',
-    policies: 'policies',
+    policies: CLAUDIA_LAMBDA_POLICIES,
     profile: config.aws.profile || 'claudia',
     timeout: 9,
     'aws-delay': 5000,
@@ -41,7 +47,7 @@ async function destroy() {
 }
 
 
-async function getDeploymentDetails(){
+async function getDeploymentDetails() {
   return fs.readJson('./build/deployment-details.json');
 }
 
